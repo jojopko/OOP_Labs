7c673973dd887f0f0a12adcda38c37d80fdc8826
jojopko <gamerat.github@gmail.com>
2023-10-10 22:21:12 +0300

    lab6

---
+++ Lab6/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Set
+{
+    private List<int> elements;
+
+    public Set(params int[] elements)
+    {
+        this.elements = new List<int>(elements);
+    }
+
+    public static Set operator &(Set set1, Set set2)
+    {
+        var result = new Set();
+        foreach (int element in set1.elements)
+        {
+            if (set2.elements.Contains(element))
+            {
+                result.elements.Add(element);
+            }
+        }
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return "{" + string.Join(", ", this.elements) + "}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Set set1 = new Set(1, 2, 3, 4, 5);
+        Set set2 = new Set(4, 5, 6, 7, 8);
+
+        Set intersection = set1 & set2;
+
+        Console.WriteLine(intersection);
+    }
+}
